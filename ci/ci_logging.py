c340a99ce4aa0e11348d546f3aeec540d7940b60
Daniel King <daniel.zidan.king@gmail.com>
2018-08-29 10:38:38 -0400

    do not duplicate messages

--- ci/ci_logging.py
+++ ci/ci_logging.py
@@ -13,11 +13,8 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 ch.setFormatter(fmt)
 
-
 log = logging.getLogger('ci')
 log.setLevel(logging.INFO)
-log.addHandler(fh)
-log.addHandler(ch)
 
 logging.basicConfig(
     handlers=[fh, ch],
