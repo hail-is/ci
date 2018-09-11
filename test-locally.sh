cb3686155539572efc0a3fc201d3c174634535f2
Daniel King <daniel.zidan.king@gmail.com>
2018-09-11 09:52:16 -0400

    fixes

--- test-locally.sh
+++ test-locally.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
 set -ex
 
-REPO_NAME=ci-test-$(LC_CTYPE=C LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c 8)
+export REPO_NAME=ci-test-$(LC_CTYPE=C LC_ALL=C tr -dc 'a-z0-9' < /dev/urandom | head -c 8)
+
+set +x
+TOKEN=$(cat github-tokens/user1)
+set -x
 
 cleanup() {
     set "" INT TERM
     set +e
     kill $(cat ci.pid)
     rm -rf ci.pid
+    set +x
     curl -XDELETE \
-         https://api.github.com/orgs/hail-is/${REPO_NAME} \
-         -H "Authorization: token $(cat github-tokens/user1)"
+         https://api.github.com/repos/hail-ci-test/${REPO_NAME} \
+         -H "Authorization: token ${TOKEN}"
+    set -x
 }
 trap cleanup EXIT
 
 trap "exit 24" INT TERM
 
+set +x
 curl -XPOST \
-     https://api.github.com/orgs/hail-is/repos \
-     -H "Authorization: token $(cat github-tokens/user1)" \
-     -d "{ \"name\" : ${REPO_NAME} }"
+     https://api.github.com/orgs/hail-ci-test/repos \
+     -H "Authorization: token ${TOKEN}" \
+     -d "{ \"name\" : \"${REPO_NAME}\" }"
+set -x
 
+rm -rf /tmp/foo
 git clone https://github.com/hail-is/ci-test.git /tmp/foo
 pushd /tmp/foo
+set +x
 git remote add new-origin \
-    https://$(cat github-tokens/user1)@github.com/hail-is/${REPO_NAME}.git
-git fetch new-origin
-git push origin new-origin/master:master
+    https://${TOKEN}@github.com/hail-ci-test/${REPO_NAME}.git
+set -x
+git push new-origin master:master
 popd
 
-export WATCHED_TARGETS='[["hail-is/'${REPO_NAME}':master", true]]'
+set +x
+./setup-endpoints.sh hail-ci-test/${REPO_NAME} ${TOKEN} ${SELF_HOSTNAME}
+set -x
+
+export WATCHED_TARGETS='[["hail-ci-test/'${REPO_NAME}':master", true]]'
 
 source activate hail-ci
 python ci/ci.py & echo $! > ci.pid
