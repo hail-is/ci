7421c101a6aee5a0f611cc630c5ecc893bc40c24
Daniel King <daniel.zidan.king@gmail.com>
2018-09-24 15:09:39 -0400

    fixes

--- hail-ci-build.sh
+++ hail-ci-build.sh
@@ -2,12 +2,4 @@
 
 set -ex
 
-cp /secrets/user* github-tokens
-
-mkdir oauth-token
-cp /secrets/oauth-token oauth-token
-
-mkdir gcloud-token
-cp /secrets/hail-ci-0-1.key gcloud-token
-
 make test-in-cluster
