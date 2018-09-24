f8f534500b8dc41b8b1bd3b46cc5047e075cdf89
Daniel King <daniel.zidan.king@gmail.com>
2018-09-24 15:04:21 -0400

    copy secrets to appropriate locations for tests

--- hail-ci-build.sh
+++ hail-ci-build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+set -ex
+
+cp /secrets/user* github-tokens
+
+mkdir oauth-token
+cp /secrets/oauth-token oauth-token
+
+mkdir gcloud-token
+cp /secrets/hail-ci-0-1.key gcloud-token
+
 make test-in-cluster
